2b8ad7d035d6c332d01cd7487db3b64919fd769f
Justin <justin.koningsberger@gmail.com>
2020-06-03 18:27:10 +0200

    Test for activity_exceeded? method

--- spec/work_ease_spec.rb
+++ spec/work_ease_spec.rb
@@ -28,6 +28,7 @@ RSpec.describe WorkEase do
   before(:each) do
     @w = WorkEase.new
     @w.testing = true
+    @time = Time.at(1591192757)
   end
 
   describe '#start' do
@@ -48,22 +49,45 @@ RSpec.describe WorkEase do
     end
   end
 
-  describe '#check' do
-    it 'sends a warning if bodypart has been too active' do
-      time = Time.at(1591192757)
+  describe '#activity_exceeded?' do
+    it 'returns false if bodypart has not been too active' do
+      Timecop.freeze(@time)
+      bodypart_activity[:voice][:activity_level] = 1
+      bodypart_activity[:voice][:high_activity_start] = @time.to_i - 19
+      bodypart_activity[:voice][:last_activity] = @time.to_i
       @w.bodypart = bodypart_activity
 
-      Timecop.freeze(time)
+      var = @w.activity_exceeded?(:voice)
+      expect(var).to eq(false)
+      Timecop.return
+    end
+
+    it 'returns true if bodypart has been too active' do
+      bodypart_activity[:voice][:activity_level] = 1
+      bodypart_activity[:voice][:high_activity_start] = @time.to_i - 20
+      bodypart_activity[:voice][:last_activity] = @time.to_i
+      @w.bodypart = bodypart_activity
+
+      var = @w.activity_exceeded?(:voice)
+      expect(var).to eq(true)
+    end
+  end
+
+  describe '#check' do
+    it 'sends a warning if bodypart has been too active' do
+      @w.bodypart = bodypart_activity
+
+      Timecop.freeze(@time)
       @w.check(:feet)
-      Timecop.freeze(time + 4)
+      Timecop.freeze(@time + 4)
       @w.check(:feet)
-      Timecop.freeze(time + 8)
+      Timecop.freeze(@time + 8)
       @w.check(:feet) #8 seconds
-      Timecop.freeze(time + 12)
+      Timecop.freeze(@time + 12)
       @w.check(:feet)
-      Timecop.freeze(time + 16)
+      Timecop.freeze(@time + 16)
       @w.check(:feet)
-      Timecop.freeze(time + 20)
+      Timecop.freeze(@time + 20)
       @w.check(:feet) #20 seconds, should warn now
 
       fixture =  ["2020-06-03 15:59:37 +0200 - You should give your feet a break, wait 5 seconds\n"]

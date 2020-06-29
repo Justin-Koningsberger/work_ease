36ecc1b41d6f5ef804f303961b09208cdad25205
Justin <justin.koningsberger@gmail.com>
2020-06-29 16:18:46 +0200

    Start of setting profiles using optparse

--- test_script.rb
+++ test_script.rb
@@ -1,6 +1,7 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
 require_relative 'workease.rb'
 
 bodypart_activity = {
@@ -20,6 +21,40 @@ bodypart_activity = {
            max_exertion: 20,
            activity_start: nil }
 }
+
+if ARGV.empty?
+  puts "Settings profile argument missing.\n\r"
+  ARGV << '-h'
+end
+
+OptionParser.new do |opts|
+  opts.banner = 'Usage: ./test_script.rb [option], do not use more than 1 option, or they will override each other'
+
+  opts.on('-0', '--profile-0', 'Standard profile') do |_o|
+  end
+
+  opts.on('-1', '--profile-1', 'Barely any typing') do |_o|
+    bodypart_activity[:hands][:min_rest] = 10
+    bodypart_activity[:hands][:max_exertion] = 5
+    bodypart_activity[:feet][:min_rest] = 3
+    bodypart_activity[:feet][:max_exertion] = 70
+    bodypart_activity[:voice][:min_rest] = 5
+    bodypart_activity[:voice][:max_exertion] = 40
+  end
+
+  opts.on('-2', '--profile-2', 'Very limited voice activity') do |_o|
+    # Do something to bodypart_activity[:voice]
+  end
+
+  opts.on('-3', '--profile-3', 'Limit feet pedal activity') do |_o|
+    # Do something to bodypart_activity[:feet]
+  end
+
+  opts.on('-h', '--help', 'Show options') do
+    puts opts
+    exit
+  end
+end.parse!
 
 keyboard_id, mouse_id = WorkEase.find_device_ids(keyboard_name: 'VirtualBox USB Keyboard', mouse_name: 'VirtualBox mouse integration')
 

28171485c1f2cfc6258759420ec3e874fac639f9
Justin <justin.koningsberger@gmail.com>
2020-07-23 15:14:16 +0200

    Finished workease profiles

--- test_script.rb
+++ test_script.rb
@@ -23,13 +23,13 @@ bodypart_activity = {
 }
 
 if ARGV.empty?
-  puts "Profile setting argument missing, -h for help, starting in standard profile."
+  puts 'Profile setting argument missing, -h for help, starting in standard profile.'
 end
 
 OptionParser.new do |opts|
-  opts.banner = 'Usage: ./test_script.rb [option], do not use more than 1 option, or they will override each other'
+  opts.banner = 'Usage: ./test_script.rb [option], do not use more than 1 option, because they can override each other'
 
-  opts.on('-1', '--profile-1', 'Barely any typing') do |_o|
+  opts.on('-1', '--profile-1', 'Barely any use of hands, higher threshold for other activity') do |_o|
     bodypart_activity[:hands][:min_rest] = 10
     bodypart_activity[:hands][:max_exertion] = 5
     bodypart_activity[:feet][:min_rest] = 3
@@ -38,12 +38,27 @@ OptionParser.new do |opts|
     bodypart_activity[:voice][:max_exertion] = 40
   end
 
-  opts.on('-2', '--profile-2', 'Very limited voice activity') do |_o|
-    # Do something to bodypart_activity[:voice]
+  opts.on('-2', '--profile-2', 'Limited voice activity, higher threshold for other activity') do |_o|
+    bodypart_activity[:voice][:min_rest] = 20
+    bodypart_activity[:voice][:max_exertion] = 10
+    bodypart_activity[:feet][:min_rest] = 3
+    bodypart_activity[:feet][:max_exertion] = 70
+    bodypart_activity[:hands][:min_rest] = 3
+    bodypart_activity[:hands][:max_exertion] = 30
   end
 
-  opts.on('-3', '--profile-3', 'Limit feet pedal activity') do |_o|
-    # Do something to bodypart_activity[:feet]
+  opts.on('-3', '--profile-3', 'Limit feet pedal activity, higher threshold for other activity') do |_o|
+    bodypart_activity[:feet][:min_rest] = 10
+    bodypart_activity[:feet][:max_exertion] = 25
+    bodypart_activity[:voice][:min_rest] = 5
+    bodypart_activity[:voice][:max_exertion] = 40
+    bodypart_activity[:hands][:min_rest] = 3
+    bodypart_activity[:hands][:max_exertion] = 30
+  end
+
+  opts.on('-4', '--profile-4', 'Burst of feet activity, requires long break') do |_o|
+    bodypart_activity[:feet][:min_rest] = 3600
+    bodypart_activity[:feet][:max_exertion] = 600
   end
 
   opts.on('-h', '--help', 'Show options') do
